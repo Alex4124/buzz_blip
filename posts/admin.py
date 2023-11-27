cb5a017fec56ab1197f924b1912d6144e03b3bcf
Alex4124 <leskinen01@mail.ru>
2023-11-27 11:13:27 +0300

    fixed css and admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('text', 'published')
+    list_display_links = ['text']
+    search_fields = ('text', 'published')
+
+
+admin.site.register(Post, PostAdmin)
